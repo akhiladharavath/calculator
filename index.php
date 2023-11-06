d0a8a539afdb2e47b2b0cffe1abfa4628e2f5e8d
akhila <akhiladharavath69@gmail.com>
2023-11-06 08:50:35 +0530

    new files are added

--- index.php
+++ index.php
@@ -9,6 +9,11 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
         <title></title>
     </head>
     <body>
+    ^7a7b22c (John Doe 2021-01-15 12:30:45 +0200 1) This is the first line.
+^ce827d5 (Jane Smith 2021-01-17 14:20:30 +0200 2) This is the second line.
+^4f6e5d7 (John Doe 2021-01-20 09:45:22 +0200 3) This is the third line.
+...
+
         <?php
         
         ?>
